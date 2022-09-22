8ddfc2e79d73216ebbf2f04a302c7eb57ea004bb
jota <aleph0528@gmail.com>
2022-09-23 06:43:55 +0900

    refactor: スペースをタブに変換

--- main.go
+++ main.go
@@ -9,16 +9,16 @@ import (
 
 func main() {
 	godotenv.Load(".env")
-    router := gin.Default()
-    router.LoadHTMLGlob("templates/*.html")
+		router := gin.Default()
+		router.LoadHTMLGlob("templates/*.html")
 
-    router.GET("/", func(ctx *gin.Context){
-        ctx.HTML(200, "index.html", gin.H{})
-    })
+		router.GET("/", func(ctx *gin.Context){
+			ctx.HTML(200, "index.html", gin.H{})
+		})
 
 	router.GET("/index", func(ctx *gin.Context) {
 		controller.Index(ctx)
 	})
 
-    router.Run()
+	router.Run()
 }
