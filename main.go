13d072d5aad127d0289d1d1d795e5c3988df415a
jota <aleph0528@gmail.com>
2022-09-23 06:44:25 +0900

    refactor: インデントがおかしかったので修正

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 	godotenv.Load(".env")
-		router := gin.Default()
-		router.LoadHTMLGlob("templates/*.html")
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
 
-		router.GET("/", func(ctx *gin.Context){
-			ctx.HTML(200, "index.html", gin.H{})
-		})
+	router.GET("/", func(ctx *gin.Context){
+		ctx.HTML(200, "index.html", gin.H{})
+	})
 
 	router.GET("/index", func(ctx *gin.Context) {
 		controller.Index(ctx)
