5fc113ac40b92ee228aaf370e59cc93a1c5456a4
jota <aleph0528@gmail.com>
2022-09-24 22:08:46 +0900

    update: dbのmodel & migrate追加

--- main.go
+++ main.go
@@ -2,13 +2,37 @@ package main
 
 import (
 	"george0528/go-notion.git/controller"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
+
+	_ "github.com/mattn/go-sqlite3"
 )
+
+type Schedule struct {
+	gorm.Model
+  DbId string
+  Name string
+  FirstDay time.Time
+  Interval int
+	Time int
+}
+
+//DBマイグレート
+func dbInit() {
+	db, err := gorm.Open("sqlite3", "sqlite.db")
+	if err != nil {
+		panic("データベース開けず！（dbInit）")
+	}
+	db.AutoMigrate(&Schedule{})
+	defer db.Close()
+}
 
 func main() {
 	godotenv.Load(".env")
+	dbInit()
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*.html")
 
